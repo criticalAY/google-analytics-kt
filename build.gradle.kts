2496a5e2c5401b5d27d733fd75d53a2f8abaca21
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2026-03-24 07:51:50 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "2.3.10"
+}
+
+group = "com.criticalay"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("io.github.oshai:kotlin-logging-jvm:6.0.9")
+    implementation("org.slf4j:slf4j-api:2.0.13")
+
+
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(22)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
