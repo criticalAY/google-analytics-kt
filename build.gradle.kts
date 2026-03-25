38fc01f0ed05d835d96a7987877e9bba8e934b4a
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2026-03-25 11:49:27 +0530

    chore: add kt serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.3.10"
     id("com.vanniktech.maven.publish") version "0.36.0"
+    kotlin("plugin.serialization") version "2.3.10"
 }
 
 group = "io.github.criticalay"
